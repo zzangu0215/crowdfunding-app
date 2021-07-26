51ade93f6159a30b7fd035e549fd6dfabec5df6e
Sam Lloyd <bobdoe784@gmail.com>
2021-07-26 12:59:49 -0700

    Home route made for loggin in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,38 @@
 const router = require("express").Router();
 const { User, Project } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      include: [
+        {
+          model: Project,
+        },
+      ],
+      attributes: { exclude: ["password"] },
+      order: [["name", "ASC"]],
+    });
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.sender("homepage", {
+      users,
+
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If user logged they are redirected to homepage
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
